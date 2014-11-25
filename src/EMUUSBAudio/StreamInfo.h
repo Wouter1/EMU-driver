8d1a482af102ae663167e29431fb9c90a98cebb7
Wouter1 <W.Pasman@TUDelft.nl>
2014-11-25 19:43:11 +0100

    ý5:q

---
+++ src/EMUUSBAudio/StreamInfo.h
@@ -0,0 +1,14 @@
+//
+//  StreamInfo.h
+//  EMUUSBAudio
+//
+//  Created by Wouter Pasman on 25/11/14.
+//  Copyright (c) 2014 com.emu. All rights reserved.
+//
+
+#ifndef EMUUSBAudio_StreamInfo_h
+#define EMUUSBAudio_StreamInfo_h
+
+
+
+#endif
